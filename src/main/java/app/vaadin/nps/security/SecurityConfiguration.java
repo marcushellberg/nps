48518db2fc3551e767c2c6be024105194cd8f58e
Marcus Hellberg <marcus@vaadin.com>
2024-11-07 14:37:31 -0800

    more k8s than anyone should ever need

--- src/main/java/app/vaadin/nps/security/SecurityConfiguration.java
+++ src/main/java/app/vaadin/nps/security/SecurityConfiguration.java
@@ -4,12 +4,10 @@ import app.vaadin.nps.views.LoginView;
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@Profile("dev")
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends VaadinWebSecurity {
